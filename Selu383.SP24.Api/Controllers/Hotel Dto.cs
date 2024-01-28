2bdecf18965da461815993ba857022376cd44b7d
Zachary Duncan <zachary.duncan@selu.edu>
2024-01-27 18:52:12 -0600

    Added Hotel Dto

---
+++ Selu383.SP24.Api/Controllers/Hotel Dto.cs
@@ -0,0 +1,22 @@
+﻿namespace Selu383.SP24.Api.Controllers
+{
+    public class HotelDto
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+    }
+
+    public class HotelCreateDto
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+    }
+
+    public class HotelUpdateDto
+    {
+        public string name { get; set; }
+        public string address { get; set; }
+    }
+}
