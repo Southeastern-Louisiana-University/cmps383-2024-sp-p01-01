87b3acbbc088e83a50bdced82381e07856e4680b
Zachary Duncan <zachary.duncan@selu.edu>
2024-01-27 20:10:54 -0600

    Updated the Hotel Dto

--- Selu383.SP24.Api/Dto/Hotel Dto.cs
+++ Selu383.SP24.Api/Dto/Hotel Dto.cs
@@ -1,5 +1,12 @@
 ﻿namespace Selu383.SP24.Api.Dto
 {
+
+    public class Hotel
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string address { get; set; }
+    }
     public class HotelDto
     {
         public int id { get; set; }
@@ -17,6 +24,12 @@
     public class HotelUpdateDto
     {
         public string name { get; set; }
+        public string address { get; set; }
+    }
+
+    public class HotelDeleteDto
+    {
+        public int name { get; set; }
         public string address { get; set; }
     }
 }
