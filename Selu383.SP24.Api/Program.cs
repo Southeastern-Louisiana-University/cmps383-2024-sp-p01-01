f4629420fedc50c563a988961bfd2914d76edfe0
Bradford Parks <bradford.parks@selu.edu>
2024-01-29 17:52:45 -0600

    Edited controller

--- Selu383.SP24.Api/Program.cs
+++ Selu383.SP24.Api/Program.cs
@@ -6,13 +6,33 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// Add DbContext
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DataContext")));
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<DataContext>();
+        context.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while migrating the database.");
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
