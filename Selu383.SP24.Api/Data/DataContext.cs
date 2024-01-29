0211a63bc828d5f33f65969eac73b2202162e309
Zachary Duncan <zachary.duncan@selu.edu>
2024-01-29 15:06:25 -0600

    Fixed seeded data, added data context, renamed Hotel Dto.cs to Hotel.cs to get rid of confusion, and updated appsettings.json to reflect our database.

--- Selu383.SP24.Api/Data/DataContext.cs
+++ Selu383.SP24.Api/Data/DataContext.cs
@@ -1,6 +1,22 @@
-﻿namespace Selu383.SP24.Api.Data
-{
-    public class DataContext
+﻿using Microsoft.EntityFrameworkCore;
+namespace Selu383.SP24.Api.Data;
+
+
+    public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){
+
+        }
+
+        public DataContext(){
+
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(DataContext).Assembly);
+        }
     }
-}
